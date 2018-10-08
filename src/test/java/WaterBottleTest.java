d7b203b1b0c6238073bd46c018fdf159b014b2ff
Michael Blumenthal <bloomer25@googlemail.com>
2018-10-08 16:45:55 +0100

    finishes water bottle with tests

--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,3 +1,40 @@
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+    WaterBottle bottle;
+
+    @Before
+    public void before(){
+        bottle = new WaterBottle();
+    }
+
+    @Test
+    public void hasVolume(){
+        assertEquals(100, bottle.getVolume() );
+    }
+
+    @Test
+    public void canDrink(){
+        bottle.drink();
+        assertEquals(90, bottle.getVolume() );
+    }
+
+    @Test
+    public void canEmptyBottle(){
+        bottle.empty();
+        assertEquals(0, bottle.getVolume() );
+    }
+
+    @Test
+    public void canFillBottle(){
+        bottle.drink();
+        bottle.drink();
+        bottle.drink();
+        bottle.fill();
+        assertEquals(100, bottle.getVolume() );
+    }
+
 
 }
