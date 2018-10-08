20a4005036d6cfa8aad357cc1389a8e6febac155
Michael Blumenthal <bloomer25@googlemail.com>
2018-10-08 16:30:18 +0100

    finishes calculator with tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,18 @@
 public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b){
+        return a - b;
+    }
+
+    public int multiply(int a, int b){
+        return a * b;
+    }
+
+    public double divide(int a, int b){
+        return (a * 1.0) / (b * 1.0);
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,37 @@
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
+    Calculator calc;
+
+    @Before
+    public void before(){
+        calc = new Calculator();
+    }
+
+    @Test
+    public void canAdd(){
+        assertEquals(4, calc.add(2,2));
+    }
+
+    @Test
+    public void canSubtract(){
+        assertEquals(3, calc.subtract(7,4));
+    }
+
+    @Test
+    public void canMultiply(){
+        assertEquals(9, calc.multiply(3,3));
+    }
+
+    @Test
+    public void canDivideWithIntegerResult(){
+        assertEquals(4.0, calc.divide(8,2), 0.1);
+    }
+
+    @Test
+    public void canDivideWithDecimalResult(){
+        assertEquals(3.5, calc.divide(7,2), 0.1);
+    }
 }
