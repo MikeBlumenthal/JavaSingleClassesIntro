eb46457d7a7c634cf8a16b5820ba122cbab28cc6
Michael Blumenthal <bloomer25@googlemail.com>
2018-10-08 17:06:07 +0100

    adds methods to printer class and tests

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,31 @@
 public class Printer {
+    private int paperRemaining;
+    private int tonerRemaining;
+
+
+
+    public Printer(int initialPaper, int initialToner){
+        this.paperRemaining = initialPaper;
+        this.tonerRemaining = initialToner;
+    }
+
+    public int checkPaper(){
+        return this.paperRemaining;
+    }
+
+    public int checkToner(){
+        return this.tonerRemaining;
+    }
+
+    public void print(int pages, int copies){
+        if (this.paperRemaining >= (pages * copies)){
+            this.paperRemaining -= (pages * copies);
+        }
+    }
+
+    public void refill(int freshSheets){
+        this.paperRemaining += freshSheets;
+    }
+
+
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,2 +1,41 @@
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrinterTest {
+    Printer printer;
+
+    @Before
+    public void before(){
+        printer = new Printer(100, 50);
+    }
+
+    @Test
+    public void hasPaper(){
+        assertEquals(100, printer.checkPaper() );
+    }
+
+    @Test
+    public void canPrint(){
+        printer.print(3,10);
+        assertEquals(70, printer.checkPaper());
+    }
+
+    @Test
+    public void willNotPrintWithInsufficientPaper(){
+        printer.print(2,100);
+        assertEquals(100, printer.checkPaper() );
+    }
+
+    @Test
+    public void canRefillPaper(){
+        printer.print(4, 20);
+        printer.refill(100);
+        assertEquals(120, printer.checkPaper() );
+    }
+
+    @Test
+    public void hasToner(){
+        assertEquals(50, printer.checkToner() );
+    }
 }
